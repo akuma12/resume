8946034aaaae458a88b3c33130734a82d17b6b52
Jim Rohrer <jrohrer1@gmail.com>
2013-01-27 16:57:30 -0500

    first commit

---
+++ assets/js/resume.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+});
+
+/*var elem = $(".skills");
+var t=0; // the height of the highest element (after the function runs)
+var t_elem;  // the highest element (after the function runs)
+$(elem).each(function () {
+    $this = $(this);
+    console.log($this.outerHeight());
+    if ( $this.outerHeight() > t ) {
+        t_elem=this;
+        t=$this.outerHeight();
+  	}
+});
+
+$(elem).each(function () {
+	$(this).outerHeight(t);
+});*/
